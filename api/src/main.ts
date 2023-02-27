a2c8178207e58e2a9a43092c3dd100c85391d6f8
jnfrati <nicofrati@gmail.com>
2023-02-27 19:20:35 -0300

    Added cors to enable localhost development

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,15 +4,16 @@ import { PrismaFilter } from './prisma/prisma.filter';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: false,
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: '*',
   });
-
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
   app.useGlobalFilters(new PrismaFilter());
 
-  await app.listen(8500, 'localhost');
+  await app.listen(8500, '0.0.0.0');
 }
 bootstrap();
